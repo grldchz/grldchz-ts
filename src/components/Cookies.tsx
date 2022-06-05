4335d8874f44839b244d340e3ad897a0c43ddf85
Nathan Rudisill <nrudisill@threatconnect.com>
2022-06-05 06:37:12 -0400

    Updated cookie policy

--- src/components/Cookies.tsx
+++ src/components/Cookies.tsx
@@ -38,16 +38,16 @@ const Cookies: React.FC<Props> = ({ setProfile }) => {
 
   const content = () => (
     <noindex><div><h1>Cookie Policy</h1>
-    <h2><blockquote>By using Grilled Cheese of the Day you agree to the following cookie policy:</blockquote></h2>
+    <h2><blockquote>By using grilledcheeseoftheday.com you agree to the following cookie policy:</blockquote></h2>
     <h3><blockquote>
-    You agree Grilled Cheese of the Day can store a cookie on your device.  This first party cookie is strictly necessary for Grilled Cheese of the Day to function properly.  There are no third party tracking cookies on Grilled Cheese of the Day.
+    You agree grilledcheeseoftheday.com can store a cookie on your device.  This first party cookie is strictly necessary for grilledcheeseoftheday.com to function properly.  There are no third party tracking cookies on grilledcheeseoftheday.com.
     </blockquote></h3>
     <h1>Disclaimer</h1>
-    <h2><blockquote>By using Grilled Cheese of the Day you agree that you have read the following disclaimer:</blockquote></h2>
-    <h3><blockquote>Grilled Cheese of the Day and its components are offered for informational purposes only; Grilled Cheese of the Day shall not be responsible or liable for the accuracy, usefulness or availability of any information transmitted or made available via Grilled Cheese of the Day, and shall not be responsible or liable for any error or omissions in that information.</blockquote></h3>
+    <h2><blockquote>By using grilledcheeseoftheday.com you agree that you have read the following disclaimer:</blockquote></h2>
+    <h3><blockquote>grilledcheeseoftheday.com and its components are offered for informational purposes only; grilledcheeseoftheday.com shall not be responsible or liable for the accuracy, usefulness or availability of any information transmitted or made available via grilledcheeseoftheday.com, and shall not be responsible or liable for any error or omissions in that information.</blockquote></h3>
     <h1>Intellectual Property</h1>
-    <h2><blockquote>By using Grilled Cheese of the Day you agree that you have read the following statement:</blockquote></h2>
-    <h3><blockquote>Grilled Cheese of the Day and its original content, features, and functionality are owned by REDACTED FOR PRIVACY and are protected by international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws.</blockquote></h3>
+    <h2><blockquote>By using grilledcheeseoftheday.com you agree that you have read the following statement:</blockquote></h2>
+    <h3><blockquote>grilledcheeseoftheday.com and its original content, features, and functionality are owned by REDACTED FOR PRIVACY and are protected by international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws.</blockquote></h3>
     <h4>If you have any questions about this Agreement, please send them to admin@grilledcheeseoftheday.com</h4> 
     </div></noindex>
   );
